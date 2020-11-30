e752515847eaab49456561990eee40d523ce2518
b1bab0ba <71466832+b1bab0ba@users.noreply.github.com>
2020-11-30 12:57:03 +0300

    Update compare.cpp

--- Kamil/compare.cpp
+++ Kamil/compare.cpp
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <iostream>
 
 int main(){
     int num1 = 0, num2 = 0;
 
-    printf("Enter the first number : ");
-    scanf("%d", &num1);
+    std::cout << "Enter the first number : ";
+    std::cin >> num1;
 
-    printf("Enter the second number : ");
-    scanf("%d", &num2);
+    std::cout << "Enter the second number : ";
+    std::cin >> num2;
 
-    printf("%s", num1 > num2 ? "First is bigger than second" : "");
-    printf("%s", num1 == num2 ? "Numbers are equal" : "");
-    printf("%s", num1 < num2 ? "First is less than second" : "");
+    std::cout << (num1 > num2 ? "First is bigger than second" : "");
+    std::cout << (num1 == num2 ? "Numbers are equal" : "");
+    std::cout << (num1 < num2 ? "First is less than second" : "");
 
 }
